f0fcf8b83cfd02d541c20ec0a98c82047a1ecdb2
Karim Altir <mhd.altir@student.ehb.be>
2023-10-09 10:07:17 +0200

    Create a class "Boek"

---
+++ Boek.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boekWinkel
+{
+    public class Boek
+    {
+        public int Isbn {  get; set; }
+        public string Naam { get; set; }
+        public string Uitgever { get; set; }
+        public double prijs;
+        public double Prijs 
+        { 
+            get {  return prijs; } 
+            set
+            {
+                if (value >= 5 && value <= 50)
+                {
+                    prijs = value;
+                } else
+                {
+                    Console.WriteLine("Prijs moet tussen 5€ en 50€ zijn.");
+                }
+            } 
+        }
+
+        public Boek(string isbn, string naam, string uitgever, double prijs)
+        {
+            Isbn = isbn;
+            Naam = naam;
+            Uitgever = uitgever;
+            Prijs = prijs;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN: {Isbn}, Naam: {Naam}, Uitgever: {Uitgever}, Prijs: {Prijs}€";
+        }
+
+        public void Lees()
+        {
+            Console.WriteLine($"Je leest het boek '{Naam}'.");
+        }
+
+    }
+}
