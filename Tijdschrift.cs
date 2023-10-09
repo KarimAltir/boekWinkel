ea8a7684482e6188282031ef8bc17e93da0d074b
Karim Altir <mhd.altir@student.ehb.be>
2023-10-09 10:45:49 +0200

    Creating "Tijdschrift" with thier entity's

---
+++ Tijdschrift.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boekWinkel
+{
+    enum Verschijningsperiode
+    {
+        Dagelijks,
+        Wekelijks,
+        Maandelijks
+    }
+    class Tijdschrift : Boek
+    {
+        public Verschijningsperiode Periode { get; set; }
+
+        public Tijdschrift(string isbn, string naam, string uitgever, double prijs, Verschijningsperiode periode) 
+            : base(isbn, naam, uitgever, prijs)
+        {
+            Periode = periode;
+        }
+
+        public override string ToString()
+        {
+            return "Verschijningsperiode: " + Periode;
+        }
+
+    }
+}
