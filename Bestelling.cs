60d747de12570154995c25683b29a260976210f4
Karim Altir <mhd.altir@student.ehb.be>
2023-10-09 10:44:13 +0200

    Creating "Bestelling" with thier entity's

---
+++ Bestelling.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boekWinkel
+{
+    enum Verschijningsperiode
+    {
+        Dagelijks,
+        Wekelijks,
+        Maandelijks
+    }
+    public class Bestelling<T>
+    {
+        private static int volgnummer = 1;
+        public int Id { get; }
+        public T Item { get; }
+        public DateTime Datum { get; }
+        public int Aantal { get; set; }
+        public Verschijningsperiode? AbonnementPeriode { get; set; }
+
+        public Bestelling(T item, int aantal, Verschijningsperiode? periode = null)
+        {
+            Id = volgnummer++;
+            Item = item;
+            Datum = DateTime.Now;
+            Aantal = aantal;
+            AbonnementPeriode = periode;
+        }
+
+        public Tuple<string, int, double> Bestel()
+        {
+            double totalePrijs = 0;
+
+            if (Item is Boek boek)
+            {
+                totalePrijs = boek.Prijs * Aantal;
+                Console.WriteLine("Boek met ISBN " + boek.Isbn + " (" + boek.Naam + ") is besteld. Aantal: " + Aantal + " Totale prijs: " + totalePrijs + "€");
+            }
+            else if (Item is Tijdschrift tijdschrift)
+            {
+                totalePrijs = tijdschrift.Prijs * Aantal;
+                Console.WriteLine("Tijdschrift met ISBN " + tijdschrift.Isbn + " (" + tijdschrift.Naam + ") is besteld. Aantal: " + Aantal + ", Totale prijs: " + totalePrijs + "€, Verschijningsperiode: " + tijdschrift.Periode + ")");
+            }
+
+            return new Tuple<string, int, double>(Id.ToString(), Aantal, totalePrijs);
+        }
+
+        public event Action<string> BoekBesteld;
+
+        public void BoekBestellingBevestigen()
+        {
+            if (Item is Boek boek)
+            {
+                BoekBesteld?.Invoke("Boek met ISBN " + boek.Isbn + " (" + boek.Naam + ") is besteld.");
+            }
+        }
+    }
+}
